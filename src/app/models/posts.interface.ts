5e3bc4b8029abcc00fbd92fcd4633c6afcc41199
Josevi97 <josevi00@hotmail.com>
2022-01-23 19:15:25 +0100

    posts fixed

--- src/app/models/posts.interface.ts
+++ src/app/models/posts.interface.ts
@@ -1,21 +1,17 @@
 import { IAccount } from './accounts.interface';
-import { IComment } from './comments.interface';
 import { ICommunity } from './communities.interface';
-import { IEntrance } from './entrances.interface';
 
 export interface IPost {
 	id: number;
 	title: string;
 	account: IAccount;
 	community: ICommunity;
-	entrance: IEntrance;
-	comment: IComment;
-	createdAt: string;
+	post: IPost;
 	body: string;
+	createdAt: string;
 	votes: number;
 	comments: number;
 	sessionVoted: number;
-	post: IPost;
 	type: string;
 	responseType: string;
 }
