3722215aefa4398fe72bf5be78d9ad7317b34131
Josevi97 <josevi00@hotmail.com>
2022-03-30 22:36:39 +0200

    arrows at start implemented

--- src/app/components/unrouted/votes/votes.component.ts
+++ src/app/components/unrouted/votes/votes.component.ts
@@ -9,6 +9,7 @@ export class VotesComponent implements OnInit {
 	@Input() public onClick: Function;
 	@Input() public vote: number;
 	@Input() public votes: number;
+	@Input() public offset: boolean;
 
 	constructor() {}
 
