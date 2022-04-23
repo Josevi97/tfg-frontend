de048e368942b58231857ed46237e2ec7b5e89fb
Josevi97 <josevi00@hotmail.com>
2022-04-23 19:57:40 +0200

    some irrelevant changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,32 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+import { HttpHeaders } from '@angular/common/http';
+
 export const environment = {
-  production: true
+	production: true,
 };
+
+export const API_URI = 'https://api.josevi.tk';
+export const RESOURCES_URI = 'https://static.josevi.tk';
+
+export const httpOptions = {
+	headers: new HttpHeaders({
+		'Content-Type': 'application/json; charset=UTF-8',
+	}),
+	withCredentials: true,
+};
+
+export const httpOptionsPart = {
+	withCredentials: true,
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
